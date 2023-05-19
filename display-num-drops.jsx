8e9bc4f78111f9335097f3753c346d9eef5e65fc
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2023-05-19 16:44:02 -0400

    working as expected, but not with planned/expected approach

--- display-num-drops.jsx
+++ display-num-drops.jsx
@@ -1,43 +1,42 @@
 const keypom_contract = "v2.keypom.near";
+const userNum = Near.view(keypom_contract, "get_drop_supply_for_owner", {
+  account_id: state.user,
+});
 
 // if (!user){
 //     return "Please Sign In";
 // }
 
-// Use and manipulate state
-State.init({ 
-    num_drops: 100000,
-    user: context.accountId
+// Init state
+State.init({
+  num_drops: 0,
+  user: context.accountId,
 });
 
-
+// Do not update state.num_drops until BtnClick
 const onInputChange = ({ target }) => {
-    State.update({ 
-        user: target.value 
-    });
+  State.update({
+    user: target.value,
+    num_drops:0
+  });
 };
 
 const onBtnClick = () => {
-    // if (!state.user) {
-    //   return;
-    // }   
+  // if (!state.user) {
+  //   return;
+  // }
 
-    let tempNum = Near.view(
-        keypom_contract, 
-        "get_drop_supply_for_owner",
-        {
-            account_id: state.user,
-        }
-    );
-    State.update({
-        num_drops: tempNum,
-        user: state.user
-    });
+//   let tempNum = Near.view(keypom_contract, "get_drop_supply_for_owner", {
+//     account_id: state.user,
+//   });
+
+  State.update({
+    num_drops: userNum,
+  });
 };
 
-
 const get_user_form = (
-    <>
+  <>
     <div class="border border-black p-3">
       <label>User Account ID</label>
       <input placeholder="benji.near" onChange={onInputChange} />
@@ -46,22 +45,18 @@ const get_user_form = (
       </button>
     </div>
   </>
-)
+);
 
 // Render
 return (
-<>
-  <div class="container border border-info p-3">
-    <h3 class="text-center">
-      The number of drops for {state.user}:
-      <span class="text-decoration-underline"> {state.num_drops} </span>
-    </h3>
+  <>
+    <div class="container border border-info p-3">
+      <h3 class="text-center">
+        The number of drops for {state.user}:
+        <span class="text-decoration-underline"> {state.num_drops} </span>
+      </h3>
 
-    <p class="text-center py-2">
-      Look at that! The number of drops, stored on the NEAR blockchain.
-    </p>
-
-    {get_user_form}
-  </div>
-</>)
-
+      {get_user_form}
+    </div>
+  </>
+);
