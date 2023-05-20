6f76c6bd2a32deb4ac1287bbfb8678df742eb0d2
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2023-05-19 21:38:39 -0400

    currently works for any single account, need to add user input

--- view-my-drops.jsx
+++ view-my-drops.jsx
@@ -10,30 +10,42 @@ State.init({
   dropTypes: [],
 });
 
-const keypom_contract = "v2.keypom.testnet";
-let my_drops = Near.view(keypom_contract, "get_drop_for_owner", {
-  account_id: state.user,
+const keypom_contract = "v2.keypom.near";
+let my_drops = Near.view(keypom_contract, "get_drops_for_owner", {
+    // account_id: context.accountId,
+    account_id: "keypom.near",
 });
 
 let drop_ids = [];
 let drop_types = [];
 
+let str = "filler";
+
 for (var i = 0; i < my_drops.length; i++) {
-  let dropObj = JSON.parse(my_drops[i]);
-  drop_ids.push(dropObj.drop_id);
-  if (dropObj.hasOwnProperty("simple")) {
+  drop_ids.push(my_drops[i].drop_id);
+  if (my_drops[i].hasOwnProperty("simple")) {
     drop_types.push("Simple Drop");
-  } else if (dropObj.hasOwnProperty("nft")) {
+  } else if (my_drops[i].hasOwnProperty("nft")) {
     drop_types.push("Non-Fungible Token Drop");
-  } else if (dropObj.hasOwnProperty("ft")) {
+  } else if (my_drops[i].hasOwnProperty("ft")) {
     drop_types.push("Fungible Token Drop");
-  } else {
+  } else if (my_drops[i].hasOwnProperty("fc")) {
     drop_types.push("Function Call Drop");
+  } else {
+    drop_types.push("Unknown");
   }
 }
 
+// // Map over the array and create an array of strings with the desired elements
+// const renderedElements = drop.map(
+//   (element, index) => `Element ${index + 1}: ${element}`
+// );
+
+// // Join the array of strings into a single string
+// const renderedOutput = renderedElements.join("\n");
+
 State.update({
-  numDrops: my_drops.length,
+  //   numDrops: my_drops.length,
   dropIds: drop_ids,
   dropTypes: drop_types,
 });
@@ -45,19 +57,40 @@ State.update({
 //     </div>
 //   </>
 // );
-
+//   <h3 class="text-center">Your Drop IDs:</h3>
+//   <p>
+//     {" "}
+//     {state.dropIds.map((el, index) => {
+//       <p key={index}>{el}</p>;
+//     })}{" "}
+//   </p>
+//   <h3 class="text-center">Types of Drops:</h3>
+//   <p>
+//     {" "}
+//     {state.dropTypes.map((el, index) => {
+//       <p key={index}>{el}</p>;
+//     })}{" "}
+//   </p>
 // Render
+
 return (
   <>
     <div class="container border border-info p-3">
+      <h2 class="text-center"> LIMITED TO 50 </h2>
       <h3 class="text-center">Number of Drops:</h3>
-      <p> {my_drops.length} </p>
-      <h3 class="text-center">Your Drop IDs:</h3>
-      <p> {state.dropIds} </p>
-      <h3 class="text-center">Types of Drops:</h3>
-      <p> {state.dropTypes} </p>
-
-      {get_user_form}
+      <p class="text-center"> {my_drops.length} </p>
+      <h3 class="text-center">Your Drop's IDs:</h3>
+      <p class="text-center">
+        {state.dropIds.map((id) => (
+          <div key={id}> {id} </div>
+        ))}
+      </p>
+      <h3 class="text-center">Your Drop Types:</h3>
+      <p class="text-center">
+        {state.dropTypes.map((type) => (
+          <div key={type}> {type} </div>
+        ))}
+      </p>
     </div>
   </>
 );
